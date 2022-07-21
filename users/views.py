5ef5b5075b46ff602e1b68e62dc55cd5d5b77d6c
alina1412 <alina1412@mail.ru>
2022-07-21 10:54:52 +0300

    demo user login

--- users/views.py
+++ users/views.py
@@ -10,24 +10,66 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from .forms import LoginForm, RegisterForm
-from shelves.models import FirstReaderCreation
+from shelves.models import FirstReaderCreation, Reader
 # from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = settings.BASE_DIR
 
 
+def shelves_user_creation(id, username):
+    FirstReaderCreation.create_reader(id, username)
+
+
+def login_user(request, user):
+    login(request, user)
+    messages.success(request, 'logged in')
+
+
+def get_user_by_name(username):
+    return User.objects.filter(username=username).first()
+
+
+def get_or_create_demo_user():
+    demo_username = 'demo_djando_user_for_books'
+    demo_password = 'demo_user_password'
+    user = get_user_by_name(demo_username)
+    if not user:
+        user = User.objects.create_user(
+            username=demo_username, password=demo_password)
+        user.save()
+        shelves_user_creation(user.id, user.username)
+
+    # create_demo_user_survey(user)
+    return user
+
 
 def home_view(request):
     context = {"auth": False}
     if request.user.is_authenticated:
         context = {"auth": True}
+    else:
+        if request.method == 'POST':
+            user = get_or_create_demo_user()
+            login_user(request, user)
+            return HttpResponseRedirect(reverse('shelves:table_books'))
     return render(request, f'{BASE_DIR}/static/templates/home.html', context)
 
 
+def delete_demo_user(request):
+    if request.user.username == 'demo_djando_user_for_books':
+        try:
+            User.objects.filter(id=request.user.id).delete()
+            Reader.objects.filter(name=request.user.username).delete()
+        except Exception as e:
+            print(e)
+
+
 def logout_view(request):
+    delete_demo_user(request)
     logout(request)
     # print('logged out')
     messages.success(request, 'logged out')
@@ -52,10 +94,6 @@ def login_user_view(request):
     return render(request, f'{BASE_DIR}/static/templates/login_user.html', {'form': LoginForm()})
 
 
-
-
-def shelves_user_creation(id, username):
-    FirstReaderCreation.create_reader(id, username)
 
 # from django.contrib.auth.forms import UserCreationForm 
 
